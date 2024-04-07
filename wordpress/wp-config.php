353d0704ab7a2791f32983a54df7099b15320f15
InstaZDLL <github.105mh@8shield.net>
2024-04-07 03:43:05 +0200

    Adding wordpress pre-configured files

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'wpuser' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'wpuser' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '172.17.0.2' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',         '3LlP8qwOMWAATzTb2o1ym7ltMKPynqUfwDNO9G9BXo8=' );
+define( 'SECURE_AUTH_KEY',  'xsERuyw8uPB5jQ1IKjzF6WFv4fGvyVagMiLC/vYD0Ks=' );
+define( 'LOGGED_IN_KEY',    'KzrMbeUgQ+90hwEegJzZ2lgxjhgAnKhmC3lhTEcNgWI=' );
+define( 'NONCE_KEY',        'Ul2uSZZrDsdV+PFVfSsDXoO4vNMydHM7m9JWrgnQkps=' );
+define( 'AUTH_SALT',        'XFiusW5jBICOIkmm2WxSTBJNSUz+aASxhizFCMSO5Gg=' );
+define( 'SECURE_AUTH_SALT', 'ZnZTXeMd2oDFBIkeL/LkHZRcTuOVAiRsE3xggU8RJQs=' );
+define( 'LOGGED_IN_SALT',   '4U830qNpnVrvvV+kf++ArYYQkxRqXP4dvC7oCdzBKL4=' );
+define( 'NONCE_SALT',       'mz+5tILW1GKMGoPPwfS2f2Sp5OafOAyal6K5GZv+LV4=' );
 
 /**#@-*/
 
